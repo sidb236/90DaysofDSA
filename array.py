2f069f6bf4383195d1bdbff71277aa336f791f91
sidb236 <sid.unpredictable@gmail.com>
2023-05-20 23:58:59 +0530

    Initial Commit

--- array.py
+++ array.py
@@ -1,2 +1,28 @@
 
-#
+#Create an array in python
+
+from array import *
+import numpy as np
+
+arr1 = array('i', [1, 2, 3, 4, 5])
+arr2 = array('d', [1.2, 2.3, 3.4, 4.5, 5.6])
+
+print(arr1)
+print(arr2)
+
+#traverse an Array
+
+def traverseArray(array):
+    for i in array:
+        print(i)
+
+traverseArray(arr1)
+
+#insert into a array
+my_array1 = array('i', [1, 2, 3, 4])
+print(my_array1)
+
+my_array1.insert(0, 7)
+print(my_array1)
+
+def access
