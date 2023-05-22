7d5b0e6ffa44ed19157b91795435bf5c59725592
sidb236 <sid.unpredictable@gmail.com>
2023-05-22 13:35:09 +0530

    Added linear_search in array.py

--- array.py
+++ array.py
@@ -13,12 +13,13 @@ print(arr2)
 # traverse an Array
 
 
-def traverseArray(array):
-    for i in array:
+def traversearray(arr):
+    for i in arr:
         print(i)
 
 
-traverseArray(arr1)
+# test the function
+traversearray(arr1)
 
 # insert into a array
 my_array1 = array('i', [1, 2, 3, 4])
@@ -29,9 +30,30 @@ print(my_array1)
 
 
 def accessElement(array, index):
-    if index> len(array):
-        print("The index provided exceeds the length of array")
+    if index> len(array):       # O(1)
+        print("The index provided exceeds the length of array")               # O(1)
     else:
         print(array[index])
 
-accessElement(arr1, 6)
+
+accessElement(arr1,6)
+
+# Searching in an Array
+# Linear Search, we iterate through the elements of array one by one,
+# comparing each element with the target value.
+
+arr1 = array('i', [1, 2, 3, 4])
+
+
+def linear_search(arr1, target):
+    for i in range(len(arr1)):
+        if arr1[i] ==target:
+            print("Found the element {} at {}".format(target, i))
+
+    return -1     #You can also raise ValueError(" Element {} not found in the array".format(target)),
+                  # you can also return False
+
+
+# Input Value in the linear_search function
+linear_search(arr1, 3)
+
