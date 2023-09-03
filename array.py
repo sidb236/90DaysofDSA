541fb2f0140ee06d792bf4dd7ac2d49404c90375
sidb236 <sid.unpredictable@gmail.com>
2023-09-04 00:14:32 +0530

    Added new functions in array module for additional information

--- array.py
+++ array.py
@@ -1,11 +1,14 @@
 
 # Create an array in python
 
+
 from array import *
 import numpy as np
 
+
 arr1 = array('i', [1, 2, 3, 4, 5])
 arr2 = array('d', [1.2, 2.3, 3.4, 4.5, 5.6])
+
 
 print(arr1)
 print(arr2)
